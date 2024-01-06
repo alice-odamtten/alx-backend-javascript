90f0ee033eadd903ac96eac9f02806768242da34
alice-odamtten <sakyibeaodamtten@gmail.com>
2024-01-06 17:12:13 +0000

    inheritance

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,12 +1,16 @@
 export default class Building {
   construictor(sqft) {
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.cnstructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new TypeError('Classes extending Building must implement evacuationWarningMessage method.');
     }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
   }
 }
