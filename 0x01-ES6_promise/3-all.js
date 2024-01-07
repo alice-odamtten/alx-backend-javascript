7c339ee667763ae5b4836296e7c640b8c28617a1
alice-odamtten <sakyibeaodamtten@gmail.com>
2024-01-07 23:55:23 +0000

    promise.all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([upload, create]) => {
+      console.log(`${upload.body} ${create.firstName} ${create.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
