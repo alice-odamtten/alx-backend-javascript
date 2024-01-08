cfd69d6f45489687ce57df64a0beb524cb1deee0
alice-odamtten <sakyibeaodamtten@gmail.com>
2024-01-08 02:54:37 +0000

    catching error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,9 +1,9 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-   queue.push(mathFunction(), 'Guardrail was processed');
+    queue.push(mathFunction(), 'Guardrail was processed');
   } catch (error) {
-     queue.push(`${error}`, 'Guardrail was processed');
+    queue.push(`${error}`, 'Guardrail was processed');
   }
   return queue;
 }
