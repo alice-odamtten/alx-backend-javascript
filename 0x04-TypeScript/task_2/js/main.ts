2d7628fdc52d621f84fba15c5eeaa58d003f02b1
alice-odamtten <sakyibeaodamtten@gmail.com>
2024-01-11 03:28:21 +0000

    task 6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -44,4 +44,17 @@ function createEmployee(salary: number | string): Director | Teacher {
   } else {
     return new Director();
   }
+ }
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
 }
+
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
