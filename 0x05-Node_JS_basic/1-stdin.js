1d6179a09694080164c8cc92dc35eac98aa06100
alice-odamtten <sakyibeaodamtten@gmail.com>
2024-02-27 01:43:09 +0000

    task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+/**
+ * function that will be executed through command line
+ */
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  if (name) {
+    console.log('Your name is:', name);
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
