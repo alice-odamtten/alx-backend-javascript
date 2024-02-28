b6cb468fb3e8bf0a7a9c00ea161a203498ad6d8e
alice-odamtten <119321981+alice-odamtten@users.noreply.github.com>
2024-02-27 18:05:27 -0800

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,25 +3,32 @@
  */
 
 const http = require('http');
-
-const url = require("url");
-
-const path = process.argv
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-  res.write('Hello Holberton School!');
-  
-  const reqUrl = url.parse(req.url).pathname
-  if(reqUrl == "/") {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // Set content type to plain text
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Parse the URL path
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  // Handle different URL paths
+  if (url.pathname === '/') {
     res.end('Hello Holberton School!\n');
+  } else if (url.pathname === '/students') {
+    const dbName = process.argv[2];
+    countStudents(dbName)
+      .then(() => {
+        res.write('This is the list of our students\n');
+        res.end();
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  } else {
+    // Handle other URL paths
+    res.end('Invalid endpoint\n');
   }
-  else if (reqUrl == "/students"){
-    res.setHeader("Content-Type", "text/csv");
-    res.setHeader("Content-Disposition", path[2]);
-    res.write("This is the list of our students");
-    res.end(countStudents(path[2]);)
 });
 
 app.listen(1245, () => {});
