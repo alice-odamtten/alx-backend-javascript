3e2115d9bf624bb343b6148f3718ca1646c9f54d
alice-odamtten <119321981+alice-odamtten@users.noreply.github.com>
2024-02-27 18:07:02 -0800

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,36 +3,12 @@
  */
 
 const http = require('http');
-const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-  // Set content type to plain text
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  // Parse the URL path
-  const url = new URL(req.url, `http://${req.headers.host}`);
-
-  // Handle different URL paths
-  if (url.pathname === '/') {
-    res.end('Hello Holberton School!\n');
-  } else if (url.pathname === '/students') {
-    const dbName = process.argv[2];
-    countStudents(dbName)
-      .then(() => {
-        res.write('This is the list of our students\n');
-        res.end();
-      })
-      .catch((error) => {
-        res.end(error.message);
-      });
-  } else {
-    // Handle other URL paths
-    res.end('Invalid endpoint\n');
-  }
+  res.end('Hello Holberton School!\n');
 });
 
 app.listen(1245);
-
-
 
 module.exports = app;
