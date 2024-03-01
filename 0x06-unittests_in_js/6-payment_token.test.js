d19643d7ac5a092c08a7b0e0bc59422c0e3585d5
alice-odamtten <119321981+alice-odamtten@users.noreply.github.com>
2024-02-29 17:45:38 -0800

    Update 6-payment_token.test.js

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,14 @@
+//comments
+
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const assert = require('assert');
+
+describe('getPaymentTokenFromAPI', () => {
+    it('testing getPaymentTokenFromAPI', (done) => {
+        getPaymentTokenFromAPI(true)
+        .then((res) => {
+            assert.deepStrictEqual(res, {data: 'Successful response from the API' })
+            done()            
+        })
+    })
+})
