765474b3827a03919cee707d42c4e58f436e4b91
alice-odamtten <119321981+alice-odamtten@users.noreply.github.com>
2024-03-27 18:41:46 -0700

    Delete 0x06-unittests_in_js directory

--- 0x06-unittests_in_js/0-calcul.js
+++
@@ -1,9 +0,0 @@
-/**
- * return the sum of two digits
- */
-
-function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
-}
-
-module.exports = calculateNumber;
--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,9 +0,0 @@
-/**
- * function will take an argument called success (boolean)
- */
-
-function getPaymentTokenFromAPI(success){
-    if (success)
-        return Promise.resolve({data: 'Successful response from the API' })
-}
-module.exports = getPaymentTokenFromAPI;
